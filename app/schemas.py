e3a1f826691105837f4fa20ecfc28b4b1c457f22
akshay-moolath <akmoolath@gmail.com>
2025-12-17 05:56:44 +0530

    added order schemas

--- app/schemas.py
+++ app/schemas.py
@@ -20,3 +20,28 @@ class UserOut(BaseModel):
 class LoginSchema(BaseModel):
     username: str
     password: str
+
+
+class OrderCreate(BaseModel):
+    amount:float
+
+
+class OrderOut(BaseModel):
+    id:int
+    user_id:int
+    amount:float
+    status:str
+    currency: str
+    created_at:datetime
+
+class OrderItemCreate(BaseModel):
+    product_id: int
+    quantity: int
+
+class OrderItemOut(BaseModel):
+    id: int
+    order_id: int
+    product_id: int
+    quantity: int
+    price_at_purchase: float
+
