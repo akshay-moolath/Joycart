127005ce92c46d21f5935fa9f119a8a322ce54c4
akshay-moolath <akmoolath@gmail.com>
2025-12-23 14:22:17 +0530

    modified payment status page to accomodate payment ids. added payment details link to orders page

--- app/payments.py
+++ app/payments.py
@@ -38,8 +38,8 @@ def payment(order_id: int, db: Session = Depends(get_db)):
         order_id=order.id,
         amount=order.amount,
         status="success",
-        gateway="mock",
-        gateway_payment_id= f"MOCK-{uuid.uuid4().hex[:12]}"
+        gateway="mockpay",
+        gateway_payment_id= f"PAY-{uuid.uuid4().hex[:12]}"
     )
 
     db.add(payment)
@@ -55,21 +55,56 @@ def payment(order_id: int, db: Session = Depends(get_db)):
 
 
 @router.get("/status/{order_id}")
-def payment_success(request: Request, order_id: int, db: Session = Depends(get_db)):
+def payment_success(
+    request: Request,
+    order_id: int,
+    db: Session = Depends(get_db)
+):
     order = db.query(Order).filter(Order.id == order_id).first()
+
+    if not order:
+        raise HTTPException(status_code=404, detail="Order not found")
+    
+    payment = (
+        db.query(Payment)
+        .filter(
+            Payment.order_id == order.id,
+            Payment.status == "success"
+        )
+        .first()
+    )
+
+    refund = (
+        db.query(Payment)
+        .filter(
+            Payment.order_id == order.id,
+            Payment.status == "refund"
+        )
+        .first()
+    )
 
     if order.status == "PAID":
         details = "Payment Success"
+        payment_id = payment.gateway_payment_id if payment else ""
+
     elif order.status == "REFUNDED":
         details = "Refund Success"
+        payment_id = refund.gateway_payment_id if refund else ""
+
+    elif order.status == "PENDING":
+        details = "Order Pending"
+        payment_id = "Payment Not done yet"
+
     else:
         details = "Order Cancelled"
+        payment_id = "No Payment ID"
 
     return templates.TemplateResponse(
         "payment_status.html",
-        {   "request":request,
+        {
+            "request": request,
             "status": order.status,
-            "details": details
-            
+            "details": details,
+            "paymentID": payment_id
         }
     )
